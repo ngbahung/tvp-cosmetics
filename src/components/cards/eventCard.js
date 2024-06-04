8800512b4e756d5fdfacba910981512b2a2a9235
Nguyen Ba Hung <22520512@gm.uit.edu.vn>
2024-06-04 23:59:30 +0700

    Update eventCard.js

--- src/components/cards/eventCard.js
+++ src/components/cards/eventCard.js
@@ -1,15 +1,17 @@
 import { Card, CardBody, CardHeader } from "@nextui-org/react";
 import EventDetailCard from "./eventDetailCard";
 
-export default function EventCard(){
+export default function EventCard({month1, day1, eventName1, dateTime1, month2, day2, eventName2, dateTime2}){
+
+
     return(
         <Card className="p-2">
             <CardHeader className="flex justify-center">
                 <h4 className="font-extrabold text-2xl ">SỰ KIỆN</h4>
             </CardHeader>
             <CardBody className="gap-4">
-                <EventDetailCard month="SEP" day={12} eventName={"ngủ"} dateTime={"14/2/2024 9:30"}/>
-                <EventDetailCard month="SEP" day={12} eventName={"ngủ"} dateTime={"14/2/2024 9:30"}/>
+                <EventDetailCard month={month1} day={day1} eventName={eventName1} dateTime={dateTime1}/>
+                <EventDetailCard month={month2} day={day2} eventName={eventName2} dateTime={dateTime2}/>
             </CardBody>
         </Card>
     );
